943970981c303141e3c9d950b15612d19c3e02cf
Killian Duranti <killianduranti@gmail.com>
2026-07-21 19:36:33 +0200

    feat(auth): add current user route

--- web/src/routes/auth.routes.js
+++ web/src/routes/auth.routes.js
@@ -4,11 +4,22 @@ const authController = require(
   '../controllers/auth.controller'
 );
 
+const authMiddleware = require(
+  '../middleware/auth.middleware'
+);
+
 const router = express.Router();
 
 router.post(
   '/login',
   authController.login
+);
+
+router.get(
+  '/me',
+  authMiddleware.authenticate,
+  authMiddleware.requireAuthentication,
+  authController.getCurrentUser
 );
 
 module.exports = router;
