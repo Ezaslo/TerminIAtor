105ffc0d7affad7f9587685c367a217c9b7ac01d
Ezaslo <enzo.t.anselmo@gmail.com>
2026-08-09 17:17:51 +0200

    chore: clean legacy Terraform runtime artifacts

--- web/terraform-sessions/37947595-b01e-4245-a743-922837703544/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "openstack" {
-  # L'authentification est lue depuis les variables OS_* du fichier OpenRC OVH.
-  region = var.openstack_region
-}
--- web/terraform-sessions/f925da3d-7c4a-4e95-bbe6-68dd8347b538/security.tf
+++
@@ -1,3 +0,0 @@
-data "openstack_networking_secgroup_v2" "shared" {
-  name = "default"
-}
